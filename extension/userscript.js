6ba7ea5ddd9243bb8c117b4d6e30ed2c2cffa688
craig <craig@gmail.com>
2023-04-22 23:09:28 +0900

    fix: CORS

--- extension/userscript.js
+++ extension/userscript.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         KoShareGPT
-// @namespace    https://kosharegpt.com/
-// @version      0.1
+// @namespace    https://www.kosharegpt.com/
+// @version      0.2
 // @description  ChatGPT와의 대화를 쉽게 공유할 수 있습니다
 // @author       kosharegpt.com
 // @match        https://chat.openai.com/chat*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// @require  https://raw.githubusercontent.com/craigchoi/kosharegpt/main/index.js
 // ==/UserScript==
