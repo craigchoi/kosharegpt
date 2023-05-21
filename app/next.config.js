4351d6f9de5d395e085ea0fd111e4c65183db95b
Craig <>
2023-05-22 02:23:13 +0900

    image url add in next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -4,60 +4,61 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [
-      "avatar.vercel.sh",
-      "s.gravatar.com",
-      "lh3.googleusercontent.com",
-      "www.datocms-assets.com",
-      "avatars.dicebear.com",
-      "pbs.twimg.com",
-      "abs.twimg.com",
-      "cdn.auth0.com",
+      'avatar.vercel.sh',
+      's.gravatar.com',
+      'lh3.googleusercontent.com',
+      'www.datocms-assets.com',
+      'avatars.dicebear.com',
+      'ssl.pstatic.net',
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      'cdn.auth0.com',
     ],
   },
   async headers() {
     return [
       {
-        source: "/:slug",
+        source: '/:slug',
         headers: [
           {
-            key: "Referrer-Policy",
-            value: "no-referrer-when-downgrade",
+            key: 'Referrer-Policy',
+            value: 'no-referrer-when-downgrade',
           },
           {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
           },
         ],
       },
       {
-        source: "/api/save",
+        source: '/api/save',
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
           {
-            key: "Access-Control-Allow-Headers",
+            key: 'Access-Control-Allow-Headers',
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
         ],
       },
       {
-        source: "/api/conversations",
+        source: '/api/conversations',
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
           {
-            key: "Access-Control-Allow-Headers",
+            key: 'Access-Control-Allow-Headers',
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
         ],
       },
@@ -66,39 +67,39 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/save",
-        destination: "/api/conversations",
+        source: '/api/save',
+        destination: '/api/conversations',
       },
     ];
   },
   async redirects() {
     return [
       {
-        source: "/explore",
-        destination: "https://kosharegpt.com",
+        source: '/explore',
+        destination: 'https://kosharegpt.com',
         permanent: false,
       },
       {
-        source: "/extension",
+        source: '/extension',
         destination:
-          "https://chrome.google.com/webstore/detail/kosharegpt-chatgpt%EC%99%80%EC%9D%98-%EB%8C%80%ED%99%94%EB%A5%BC/fhdcapmddpgmhhjifgpahpgodigigcck/related?hl=en&authuser=0",
+          'https://chrome.google.com/webstore/detail/kosharegpt-chatgpt%EC%99%80%EC%9D%98-%EB%8C%80%ED%99%94%EB%A5%BC/fhdcapmddpgmhhjifgpahpgodigigcck/related?hl=en&authuser=0',
         permanent: false,
       },
       {
-        source: "/shortcut",
+        source: '/shortcut',
         destination:
-          "https://www.icloud.com/shortcuts/7d4712f9ca6e4c60aeb5cf813873dcd3",
+          'https://www.icloud.com/shortcuts/7d4712f9ca6e4c60aeb5cf813873dcd3',
         permanent: false,
       },
       {
-        source: "/github",
-        destination: "https://github.com/craigchoi/kosharegpt",
+        source: '/github',
+        destination: 'https://github.com/craigchoi/kosharegpt',
         permanent: false,
       },
       {
-        source: "/mentions",
+        source: '/mentions',
         destination:
-          "https://twitter.com/search?q=shareg.pt%20-from%3Asteventey&src=typed_query&f=top", // show ShareGPT mentions on Twitter that are not from @steventey
+          'https://twitter.com/search?q=shareg.pt%20-from%3Asteventey&src=typed_query&f=top', // show ShareGPT mentions on Twitter that are not from @steventey
         permanent: false,
       },
       // {
